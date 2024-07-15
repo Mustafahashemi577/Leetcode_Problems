3b4a36d1d2dd529ccfef931773ed233286fd39a7
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2024-07-15 04:41:58 +0430

    Add files via upload
    Add_Two_Number problem with medium difficulty level code in Java
    https://leetcode.com/problems/add-two-numbers/description/

---
+++ Add_Two_Number.java
@@ -0,0 +1,24 @@
+class Add_Two_Number {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode temp = new ListNode();
+        ListNode result = temp ;
+        int total = 0, carry = 0;
+        
+        while (l1!=null || l2 != null || carry != 0){
+            total = carry ;
+            if(l1!=null){
+                total += l1.val;
+                l1=l1.next;
+            }
+            if(l2 != null ){
+                total += l2.val;
+                l2 = l2.next;
+            }
+            int num = total%10;
+            carry = total/10;
+            temp.next = new ListNode(num);
+            temp = temp.next;
+        }
+        return result.next;       
+    }
+}
