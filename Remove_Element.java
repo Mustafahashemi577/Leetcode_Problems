c471f471455bd9bdb78943e30dca6ff4d689203e
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2024-07-21 05:00:21 +0430

    Add files via upload
    Remove_Element problem with easy difficulty code in Java
    https://leetcode.com/problems/remove-element/description/

---
+++ Remove_Element.java
@@ -0,0 +1,12 @@
+class Remove_Element {
+    public int removeElement(int[] nums, int val) {
+        int j = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] != val ){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
