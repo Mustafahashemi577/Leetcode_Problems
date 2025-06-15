ce38e1b4ca4f8335c81b4c980b1eab9665cbb414
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-06-15 12:52:36 +0430

    Add files via upload
    MergeStringsAlternately problem with easy difficulty and with 86 beats runtime code in Kotlin
    https://leetcode.com/problems/merge-strings-alternately/description/

---
+++ MergeStringsAlternately.kt
@@ -0,0 +1,11 @@
+class MergeStringsAlternately{
+    fun mergeAlternately(word1: String, word2: String): String {
+        val sb = StringBuilder()
+        val maxLength = maxOf(word1.length, word2.length)
+        for (i in 0 until maxLength) {
+            if (i < word1.length) sb.append(word1[i])
+            if (i < word2.length) sb.append(word2[i])
+        }
+        return sb.toString()
+    }   
+}
