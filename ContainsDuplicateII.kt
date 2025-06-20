a618d04551689b9bce0dc31e1c0391e7ce8665ab
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-06-20 18:45:06 +0430

    Add files via upload
    ContainsDuplicateII problem with easy difficulty and with 74% beats runtime code in Kotlin
    https://leetcode.com/problems/contains-duplicate-ii/description/

---
+++ ContainsDuplicateII.kt
@@ -0,0 +1,12 @@
+class ContainsDuplicateII {
+    fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+        val map = HashMap<Int, Int>()
+        for (i in nums.indices) {
+            if (map.containsKey(nums[i]) && i - map[nums[i]]!! <= k) {
+                return true
+            }
+            map[nums[i]] = i
+        }
+        return false
+    }
+}
