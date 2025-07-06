89fad474ba085805cd906db84ad6b4fef3dad76d
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-07-06 09:19:58 +0430

    Add files via upload
    FinalPricesWithASpecialDiscountInAShop problem with easy difficulty and 100% beats runtime code in Kotlin
    https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/

---
+++ FinalPricesWithASpecialDiscountInAShop.kt
@@ -0,0 +1,15 @@
+class FinalPricesWithASpecialDiscountInAShop {
+    fun finalPrices(prices: IntArray): IntArray {
+        val n = prices.size
+        for (i in 0 until n) {
+            for (j in i + 1 until n) {
+                if (prices[j] <= prices[i]) {
+                    prices[i] -= prices[j]
+                    break
+                }
+            }
+        }
+        return prices
+    }
+}
+
