345c8078be8ebea2c6ffc4ac4bd48f47ab40a6f0
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-07-08 17:40:31 +0430

    Add files via upload
    MinStack problem with medium difficulty code in Kotlin
    https://leetcode.com/problems/min-stack/description/

---
+++ MinStack.kt
@@ -0,0 +1,14 @@
+class MinStack {
+    fun makeGood(s: String): String {
+        val sb = StringBuilder()
+        for (c in s) {
+            val len = sb.length
+            if (len > 0 && kotlin.math.abs(sb[len - 1] - c) == 32) {
+                sb.deleteCharAt(len - 1)
+            } else {
+                sb.append(c)
+            }
+        }
+        return sb.toString()
+    }
+}
