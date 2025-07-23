b550669356968fb19bcc54e060c0f12a3c3a0fe0
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-07-23 12:48:14 +0430

    Update Sqrt_x.kt

--- Sqrt_x.kt
+++ Sqrt_x.kt
@@ -1,18 +1,23 @@
 class Sqrt_x {
-    public int mySqrt(int x) {
-        int l = 0, r = x;
-        int res = 0;
+   fun mySqrt(x: Int): Int {
+        var l = 0
+        var r = x
+        var res = 0
         while (l <= r) {
-            int m = l + (r - l) / 2;
-            if ((long) m * m > x) {
-                r = m - 1;
-            } else if ((long) m * m < x) {
-                l = m + 1;
-                res = m;
-            } else {
-                return m;
+            val m = l + (r - l) / 2
+            when {
+                m.toLong() * m > x -> {
+                    r = m - 1
+                }
+                m.toLong() * m < x -> {
+                    l = m + 1
+                    res = m
+                }
+                else -> {
+                    return m
+                }
             }
         }
-        return res;
+        return res
     }
 }
