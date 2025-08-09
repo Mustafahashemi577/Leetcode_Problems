0fd37af29e92e409406396877433a36719c8a60b
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-08-09 10:39:59 +0430

    Add files via upload
    PowerOfTwo problem of 2025-08-09 with easy difficulty and 100% beats runtime code in Kotlin
    https://leetcode.com/problems/power-of-two/description/?envType=daily-question&envId=2025-08-09

---
+++ PowerOfTwo.kt
@@ -0,0 +1,11 @@
+class PowerOfTwo {
+    fun isPowerOfTwo(n: Int): Boolean {
+        if(n<=0)
+            return false
+        var num = n
+        while(num%2==0){
+            num/=2
+        }
+        return num==1
+    }
+}
