0afdb12e2e58c3e167ad938c0389093c4bde9d51
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-08-11 08:37:54 +0430

    Add files via upload
    MaximumDepthOfBinaryTree problem with easy difficulty and with 100% beats runtime code in Kotlin
    https://leetcode.com/problems/maximum-depth-of-binary-tree/description/

---
+++ MaximumDepthOfBinaryTree.kt
@@ -0,0 +1,8 @@
+class MaximumDepthOfBinaryTree {
+    fun maxDepth(root: TreeNode?): Int {
+        if(root==null)
+            return 0
+
+        return 1+max(maxDepth(root.left),maxDepth(root.right));
+    }
+}
