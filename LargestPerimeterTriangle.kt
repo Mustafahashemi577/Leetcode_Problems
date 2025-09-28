86ae93153ac6230338d2ca8cd8305a2efc66f706
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-09-28 14:41:10 +0430

    Add files via upload
    LargestPerimeterTriangle problem of 2025-09-28 with easy difficulty and with 90.91% beats runtime code in Kotlin
    https://leetcode.com/problems/largest-perimeter-triangle/submissions/1785195782/?envType=daily-question&envId=2025-09-28

---
+++ LargestPerimeterTriangle.kt
@@ -0,0 +1,9 @@
+class LargestPerimeterTriangle {
+    fun largestPerimeter(nums: IntArray): Int {
+        nums.sort()
+        for(i in nums.size-1 downTo 2)
+            if(nums[i]<nums[i-1]+nums[i-2])
+                return nums[i]+nums[i-1]+nums[i-2]
+        return 0
+    }
+}
