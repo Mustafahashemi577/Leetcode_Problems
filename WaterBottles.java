60242e6c6b8c298bbcd02d473afbe6c3babad380
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-10-01 17:26:03 +0430

    Add files via upload
    WaterBottles problem of the 2025-10-01 with easy difficulty and with 100% beats runtime code in Java
    https://leetcode.com/problems/water-bottles/?envType=daily-question&envId=2025-10-01

---
+++ WaterBottles.java
@@ -0,0 +1,14 @@
+class WaterBottles {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int total = numBottles; 
+        int empty = numBottles;
+        
+        while (empty >= numExchange) {
+            int newBottles = empty / numExchange; 
+            total += newBottles; 
+            empty = empty % numExchange + newBottles;
+        }
+        
+        return total;
+    }
+}
