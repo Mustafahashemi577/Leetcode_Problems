4abf07f9d3f35b3048acd6c4f14ad424d45d9eec
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-10-15 11:24:59 +0430

    Add files via upload
    LongestCommonSubsequence problem with medium difficulty and with 84.88% beats runtime code in Kotlin
    https://leetcode.com/problems/longest-common-subsequence/description/

---
+++ LongestCommonSubsequence.kt
@@ -0,0 +1,16 @@
+class LongestCommonSubsequence {
+    fun longestCommonSubsequence(text1: String, text2: String): Int {
+        val dp = Array(text1.length + 1) { IntArray(text2.length + 1) }
+
+        for (i in text1.length - 1 downTo 0) {
+            for (j in text2.length - 1 downTo 0) {
+                if (text1[i] == text2[j]) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                } else {
+                    dp[i][j] = maxOf(dp[i + 1][j], dp[i][j + 1])
+                }
+            }
+        }
+        return dp[0][0]
+    }
+}
