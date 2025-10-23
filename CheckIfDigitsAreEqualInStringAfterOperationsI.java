ae2c5e1c844c4f8d2478898acc9fc18961d91a15
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-10-23 20:54:33 +0430

    Add files via upload
    CheckIfDigitsAreEqualInStringAfterOperationsI problem of 2025-10-23 with easy difficulty and with 100% beats runtime code in Java
    https://leetcode.com/problems/check-if-digits-are-equal-in-string-after-operations-i/?envType=daily-question&envId=2025-10-23

---
+++ CheckIfDigitsAreEqualInStringAfterOperationsI.java
@@ -0,0 +1,17 @@
+class CheckIfDigitsAreEqualInStringAfterOperationsI {
+  public boolean hasSameDigits(String s) {
+    int[] arr = new int[s.length()];
+
+    for (int i = 0; i < arr.length; i++)
+      arr[i] = s.charAt(i);
+    
+    for (int length = arr.length; length > 2; length--) {
+      for (int i = 0; i < length - 1; i++) {
+        arr[i] = (arr[i] + arr[i + 1]) % 10;
+      }
+    }
+
+    return arr[0] == arr[1];
+      
+  }
+}
