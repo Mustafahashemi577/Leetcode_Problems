2e6a294a41b29cc681c983c3a5d2596eb6d81ba3
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-11-07 09:23:00 +0430

    Add files via upload
    MaximizeTheMinimumPoweredCity problem of 2025-11-07 with hard difficulty code in java
    https://leetcode.com/problems/maximize-the-minimum-powered-city/?envType=daily-question&envId=2025-11-07

---
+++ MaximizeTheMinimumPoweredCity.java
@@ -0,0 +1,44 @@
+class MaximizeTheMinimumPoweredCity {
+    public long maxPower(int[] stations, int r, int k) {
+        int n = stations.length;
+        long[] count = new long[n+1];
+        for(int i=0;i<n;i++){
+            int left = Math.max(0,i-r), right = Math.min(n,i+r+1);
+            count[left] += stations[i];
+            count[right] -= stations[i];
+        }
+        long low = Arrays.stream(stations).min().getAsInt();
+        long high = Arrays.stream(stations).asLongStream().sum() + k;
+        long ans = 0;
+        while(low<=high){
+            long mid = low+(high-low)/2;
+            if(check(count,mid,r,k)){
+                ans = mid;
+                low = mid+1;
+            }
+            else{
+                high = mid - 1;
+            }
+        } 
+        return ans;
+    }
+    private boolean check(long[] count,long val,int r,int k){
+        int n = count.length - 1;
+        long[] diff = count.clone();
+        long sum = 0, remaining = k;
+        for(int i=0;i<n;i++){
+            sum += diff[i];
+            if(sum<val){
+                long add = val - sum;
+                if(remaining<add){
+                    return false;
+                }
+                remaining -= add;
+                int end = Math.min(n,i+2*r+1);
+                diff[end] -= add;
+                sum += add;
+            }
+        }
+        return true;
+    }
+}
