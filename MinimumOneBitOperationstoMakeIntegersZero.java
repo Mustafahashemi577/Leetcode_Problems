68ae1d777e43331f99988217d7080b5a9d281199
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-11-08 10:52:39 +0430

    Add files via upload
    MinimumOneBitOperationstoMakeIntegersZero problem with hard difficulty and with most beats runtime and memory code in Java
    https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero/description/?envType=daily-question&envId=2025-11-08

---
+++ MinimumOneBitOperationstoMakeIntegersZero.java
@@ -0,0 +1,7 @@
+class MinimumOneBitOperationstoMakeIntegersZero {
+    public int minimumOneBitOperations(int n) {
+        for(int k = 1;k<=16;k*=2)
+            n^=(n>>k);
+        return n;
+    }
+}
