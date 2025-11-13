ce3e2b3accb3f31d6353195301efe6eabd7567b6
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-11-13 11:47:54 +0430

    Add files via upload
    MaximumNumberOfOperationToMoveOnesToTheEnd problem of 2025-11-13 with medium difficulty code in Python3
    https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/description/?envType=daily-question&envId=2025-11-13

---
+++ MaximumNumberofOperationstoMoveOnestotheEnd.py
@@ -0,0 +1,13 @@
+class MaximumNumberofOperationstoMoveOnestotheEnd:
+    def maxOperations(self, s: str) -> int:
+        ones,res = 0,0
+        for i in range(len(s)):
+            if s[i]=='1':
+                ones+=1
+            elif i>0 and s[i-1]=='1':
+                res+=ones
+
+        return res
+
+
+    
