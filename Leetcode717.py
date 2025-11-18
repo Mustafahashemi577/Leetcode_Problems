033fa351f787163cff6e8040f837a4f75315cb87
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2025-11-18 11:13:09 +0430

    Add files via upload
    1-bit and 2-bit Characters problem of 2025-11-18 with easy difficulty and with most beats runtime and memory code in Python
    https://leetcode.com/problems/1-bit-and-2-bit-characters/description/?envType=daily-question&envId=2025-11-18

---
+++ Leetcode717.py
@@ -0,0 +1,7 @@
+class Leetcode717:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        i=0
+        while i<len(bits)-1:
+            i+=bits[i]+1
+        return i==len(bits)-1
+        
