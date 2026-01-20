3e64d0e694197d1ec95159c95a31e2158be7b39e
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2026-01-20 07:27:36 +0430

    Add files via upload
    Construct the Minimum Bitwise Array I problem of 2026-01-20 with easy difficulty and with most beats runtime and memory code in Python3
    https://leetcode.com/problems/construct-the-minimum-bitwise-array-i/description/?envType=daily-question&envId=2026-01-20

---
+++ Leetcode3314.py
@@ -0,0 +1,9 @@
+class Leetcode3314:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        for n in nums:
+            if n&1:
+                ans.append(n & ~(((n + 1) & ~n) >> 1))
+            else:
+                ans.append(-1)
+        return ans
