591934d5106ec5209c0bba986d7baf5d5bf668b3
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2026-02-14 11:33:50 +0430

    Add files via upload
    Champagne Tower problem of 2026-02-14 with medium difficulty and with 85.63% beats runtime code in Python3
    https://leetcode.com/problems/champagne-tower/description/?envType=daily-question&envId=2026-02-14

---
+++ Leetcode799.py
@@ -0,0 +1,14 @@
+class Leetcode799:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        a = [[0.0] * 102 for _ in range(102)]
+        
+        a[0][0] = float(poured)
+        
+        for r in range(query_row + 1):
+            for c in range(r + 1):
+                q = (a[r][c] - 1.0) / 2.0
+                if q > 0:
+                    a[r + 1][c] += q
+                    a[r + 1][c + 1] += q
+        
+        return min(1, a[query_row][query_glass])
