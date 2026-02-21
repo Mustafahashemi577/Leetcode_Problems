ed4896c53d99d399f4b5471c4d6bc9a1f46c918b
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2026-02-21 10:58:19 +0430

    Add files via upload
    Prime Number of Set Bits in Binary Representation problem of 2026-02-21 with easy difficulty and 90% beats runtime code in Python3
    https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation/description/?envType=daily-question&envId=2026-02-21

---
+++ Leetcode762.py
@@ -0,0 +1,4 @@
+class Leetcode762:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+       primes = {2,3,5,7,11,13,17,19,23,29,31}
+       return sum(num.bit_count() in primes for num in range(left, right + 1))
