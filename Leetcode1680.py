c4a0dac45ba5c8c0a739d4a373aa0a64dfafa66a
Mustafahashemi577 <157291518+Mustafahashemi577@users.noreply.github.com>
2026-02-28 05:46:37 +0430

    Add files via upload
    Concatenation of Consecutive Binary Numbers problem of 2026-02-28 with medium difficulty code in Python3
    https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers/description/?envType=daily-question&envId=2026-02-28

---
+++ Leetcode1680.py
@@ -0,0 +1,13 @@
+class Leetcode1680:
+    def concatenatedBinary(self, n: int) -> int:
+        MOD = 10**9+7
+        ans = 0
+        length = 0
+
+        for i in range(1,n+1):
+            if(i&(i-1))==0:
+                length+=1
+
+            ans = ((ans<<length)|i)%MOD
+        
+        return ans
